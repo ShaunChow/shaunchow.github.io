0c1bd0432886e7c5d9684224612b02296321cba8
shaun_zhou <uranus-neptune@hotmail.com>
2020-05-05 23:50:15 +0800

    [Update]

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import Layout from '../components/HydeLayout'
+import ReactMarkdown from 'react-markdown'
+
+const input = '## 赠药山高僧惟俨\n\n炼得身形似鹤形，千株松下两函经。\n\n我来问道无余说，云在青天水在瓶。'
 
 export default () => (
   <Layout title="Shaun Chow" >
-    <h1>Home</h1>
+    <ReactMarkdown source={input} />
   </Layout>
 )
